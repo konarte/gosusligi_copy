c17a7c9c6a8660ccd6a2219ebf64da0346cf7844
VeryBigSad <khromov05@gmail.com>
2021-06-24 16:27:47 +0300

    underscores in the env variable

--- gosusligi_copy/settings.py
+++ gosusligi_copy/settings.py
@@ -112,12 +112,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
